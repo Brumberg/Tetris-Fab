326dc5244d2d0a803d9998db173cc2633df40f43
as864 <alexander.sterk@venus.uni-freiburg.de>
2017-01-15 19:57:16 +0100

    GUI_finished

---
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/ErrorWindowActivity.java
@@ -0,0 +1,13 @@
+package com.example.crunky.smartminifab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ErrorWindowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_window);
+    }
+}
---
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/HelpWindowActivity.java
@@ -0,0 +1,13 @@
+package com.example.crunky.smartminifab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpWindowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_window);
+    }
+}
