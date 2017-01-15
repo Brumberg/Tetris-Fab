2ee7d10c99462fb8c7f7933f71dc0e22384a4a9b
Oliver Brumberg <Brumberg@gmx.net>
2017-01-15 02:32:35 +0100

    some comments

--- AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/TCPIPModule.java
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/TCPIPModule.java
@@ -8,6 +8,24 @@ import com.example.crunky.smartminifab.WiFiConnection;
 
 /**
  * Created by oli on 10.01.2017.
+ * TCPIPModule is based on Daniels implementation. The module should support all settings required
+ * to establish a TCPIP connection. This class is strongly related to an activity (needs to be
+ * implemented). This settings are not required by other system components. However, the module
+ * implements an interface called IFabCommunication that should be used by other components
+ * requiring access to the Arduino. Thus, implementing Bluetooth would just mean substituting
+ * TCPIPModule with a corresponding Bluetooth module implementing IFabCommunication.
+ * There may be a better description than TCPIP Module. As the module just forms a container holding
+ * all relevant TCPIP stuff it should be quite easy to adopt the naming (job for an expert).
+ *
+ * Open questions:
+ *  support of DHCP or static IP?
+ *  what happens if the Fab is busy (placing blocks)? If the job requires more than 30secs?
+ *  Does it block the GUI? Are there cyclic telegrams (up to now we have send/request pairs -
+ *  you send something and get an answer to that request. If there are cyclic requests you
+ *  could get answers that are related to different requests)?
+ *
+ * The module is just provided as it is - that means it has to be extended to take additional
+ * requirements into account (maybe Daniel can put some effort into the module).
  */
 
 public class TCPIPModule implements IFabCommunication {
