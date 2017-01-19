2de6feee4938f59381bb5b0f04df41d1701d9ca6
EdithM <edith.mannherz@gmail.com>
2017-01-19 21:03:45 +0100

    Übergänge
    Übergänge DevMode, Placement, Seedbox

--- AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/SeedBoxModeActivity.java
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/SeedBoxModeActivity.java
@@ -1,7 +1,9 @@
 package com.example.crunky.smartminifab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SeedBoxModeActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class SeedBoxModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seed_box_mode);
+    }
+
+    public void goToDevelopmentModeActivity(View view) { //is called by onClick function of Button in activity_main.xml
+        Intent intent = new Intent(this, DevelopmentModeActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToPlacementModeActivity(View view) { //is called by onClick function of Button in activity_main.xml
+        Intent intent = new Intent(this, PlacementModeActivity.class);
+        startActivity(intent);
     }
 }
--- AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/StartActivity.java
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/StartActivity.java
@@ -3,22 +3,82 @@ package com.example.crunky.smartminifab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
+    String msg = "Android : ";
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.d(msg, "The onCreate() event");
+
     }
 
-    //TODO: Tempfunction to be deleted
-    public void nextPage(View v){
-        Intent intent = new Intent(this, PlacementModeActivity.class);
+    /**
+     * Called when the activity is about to become visible.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
 
+    /**
+     * Called when the activity has become visible.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /**
+     * Called when another activity is taking focus.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /**
+     * Called when the activity is no longer visible.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /**
+     * Called just before the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
+    public void goToDevelopmentModeActivity(View view) { //is called by onClick function of Button in activity_main.xml
+        Intent intent = new Intent(this, DevelopmentModeActivity.class);
         startActivity(intent);
     }
 
+    public void goToPlacementModeActivity(View view) { //is called by onClick function of Button in activity_main.xml
+        Intent intent = new Intent(this, PlacementModeActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSeedBoxModeActivity(View view) { //is called by onClick function of Button in activity_main.xml
+        Intent intent = new Intent(this, SeedBoxModeActivity.class);
+        startActivity(intent);
+    }
 
 }
