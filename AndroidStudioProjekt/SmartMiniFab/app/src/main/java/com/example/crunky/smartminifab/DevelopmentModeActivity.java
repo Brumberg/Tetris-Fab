fd8219bc42da3b27205726eb670a7b96381f1891
EdithM <edith.mannherz@gmail.com>
2017-01-22 19:05:26 +0100

    work on DevMode

--- AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/DevelopmentModeActivity.java
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/DevelopmentModeActivity.java
@@ -3,9 +3,18 @@ package com.example.crunky.smartminifab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.TextUtils.EllipsizeCallback;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.util.Log;
+import java.lang.String;
+import java.lang.Object;
+import java.util.concurrent.locks.Condition;
+
 
 public class DevelopmentModeActivity extends AppCompatActivity {
 
@@ -24,4 +33,17 @@ public class DevelopmentModeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SeedBoxModeActivity.class);
         startActivity(intent);
     }
+
+    /* Hide ConnectButton until some kind of Passwort is tiped in*/
+
+    final Button connectButton = (Button) findViewById(R.id.Connect);
+    EditText inpPassword = (EditText) findViewById(R.id.Username);
+    public String Password = inpPassword.getText().toString();
+    int Test = inpPassword.getText().toString().trim().length();
+
+    /*if(Test==0)
+        connectButton.setVisibility(View.GONE);
+    } else {
+        connectButton.setVisibility(View.VISIBLE);
+    }*/
 }
