89274b70b6c42c9b7599875e663d10191ee8c116
Oliver Brumberg <Brumberg@gmx.net>
2017-03-03 21:09:06 +0100

    Comments added

--- AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/Block.java
+++ AndroidStudioProjekt/SmartMiniFab/app/src/main/java/com/example/crunky/smartminifab/Block.java
@@ -4,8 +4,8 @@ import android.graphics.*;
 
 /**
  * Created by Daniel on 31.12.2016.
- * Point sucks. I got an uninitialized array. Figuring out what happend took me a while
- *
+ * Point sucks. I got an uninitialized array. Figuring out what happened took me a while
+ * (Constructor of default class Point does not work as expected/specified)
  */
 class Point {
     Point () {
@@ -23,7 +23,11 @@ enum BlockRotation {DEGREES_0, DEGREES_90, DEGREES_180, DEGREES_270}
 
 public class Block extends BlockType {
     private static BlockRotation[] blockRotationValues = BlockRotation.values();
-    private BlockRotation m_rotation;
+    private BlockRotation m_rotation; //current roatation of the block
+
+    /**
+     * description of the shape of the block elements
+     */
     private static boolean[][][] shapeDataTemplates = {
             {{true}}, // SIMPLE_SQUARE
             {{true, true}, {true, true}}, // QUADRUPLE_SQUARE
@@ -34,6 +38,10 @@ public class Block extends BlockType {
             {{true, false}, {true, true}, {true, false}}, // MIRRORED_T_SHAPE
             {{true, true, false}, {false, true, true}}, // MIRRORRED_FOUR_SHAPE
     };
+
+    /**
+     * description of the center of graphity of the individual block elements
+     */
     final static Point[] centerOfGravity = {
             new Point(0, 0), // SIMPLE_SQUARE
             new Point(0, 1), // QUADRUPLE_SQUARE
@@ -47,12 +55,22 @@ public class Block extends BlockType {
 
     private Point m_position;
 
+    /**
+     * constructor defines a block that is not placed.
+     * @param shape
+     * @param color
+     * @param rotation
+     */
     public Block(BlockShape shape, BlockColor color, BlockRotation rotation) {
         super(shape, color);
         setRotation(rotation);
         m_position = null;
     }
 
+    /**
+     * get width of the block according to its current orientation
+     * @return
+     */
     public int GetWidth() {
         int width;
         switch (m_rotation) {
@@ -75,6 +93,10 @@ public class Block extends BlockType {
         return width;
     }
 
+    /**
+     * get height of individual block according to its current rotation
+     * @return
+     */
     public int GetHeight() {
         int height;
         switch (m_rotation) {
@@ -97,6 +119,10 @@ public class Block extends BlockType {
         return height;
     }
 
+    /**
+     * returns center of grapity - see specification of the individual blocks
+     * @return
+     */
     public Point getCenterOfGravity() {
         Point center = new Point();
         int lengthx = shapeDataTemplates[getShape().ordinal()].length;
@@ -128,18 +154,37 @@ public class Block extends BlockType {
         return center;
     }
 
+    /**
+     * returns position withn seed box (make sure it is placed)
+     * @param position
+     */
     public Point getPosition() {
         return m_position;
     }
 
+    /**
+     * direct assignment of position (within seed box)
+     * @param position
+     */
     public void setPosition(Point position) {
         m_position = position;
     }
 
+    /**
+     * direct assignment of orientation (e.g. seed box)
+     * @param rotation
+     */
     public void setRotation(BlockRotation rotation) {
         m_rotation = rotation;
     }
 
+    /**
+     * returns the shape of the object depending on its orientation.
+     * return value false means transparency, true means block body
+     * @param x
+     * @param y
+     * @return
+     */
     boolean GetBlockData(int x,int y) {
         boolean retVal=false;
         boolean[][] shapetemplate = shapeDataTemplates[getShape().ordinal()];
@@ -190,19 +235,37 @@ public class Block extends BlockType {
         return retVal;
     }
 
+    /**
+     * returns the current orientation of the block (needed by seed box)
+     * @return
+     */
     public BlockRotation getRotation() {
         return m_rotation;
     }
 
+    /**
+     * rotates the block clockwise
+     */
     public void rotateClockwise() {
         setRotation(blockRotationValues[(m_rotation.ordinal() + 1) % blockRotationValues.length]);
     }
 
+    /**
+     * rotates the block counterclockwise
+     */
     public void rotateCounterClockwise() {
-        setRotation(blockRotationValues[(blockRotationValues.length + m_rotation.ordinal() - 1) % blockRotationValues.length]);
+        setRotation(blockRotationValues[(blockRotationValues.length + m_rotation.ordinal() - 1) %
+                blockRotationValues.length]);
     }
 
+    /**
+     * returns a representative string that is finally part of the string to be send to the
+     * factory
+     * @return
+     */
     public String toString() {
-        return Integer.toString(getShape().ordinal()) + Integer.toString(getRotation().ordinal()) + Integer.toString(m_position.x + 1) + Integer.toString(m_position.y + 1) + Integer.toString(getColor().ordinal());
+        return Integer.toString(getShape().ordinal()) + Integer.toString(getRotation().ordinal()) +
+                Integer.toString(m_position.x + 1) + Integer.toString(m_position.y + 1) +
+                Integer.toString(getColor().ordinal());
     }
 }
